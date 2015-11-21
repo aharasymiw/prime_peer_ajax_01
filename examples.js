b94624c90a6f2c6d651f379f53fba49a9f427689
Andrew Harasymiw <aharasymiw@gmail.com>
2015-11-20 22:03:00 -0600

    Finished the peer challenge.

--- examples.js
+++ examples.js
@@ -1,40 +1,15 @@
-/**
- * Created by joelmiller on 7/14/15.
- */
-$(document).ready(function() {
+$(document).ready(function(){
 
-    $.ajax('insert.html', {
-        success: function(response){$('.myContainer').html(response)},
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
+  $.ajax({
+    url:'/data.json'
+  }) .done(function(json){
 
-    $.ajax('data.json', {
-        success: function(response){
-            var msg = $("<p></p>");
-            msg.append("Color " + response.color);
-            msg.append("Qty " + response.value);
-            $('.myContainer').html(msg)
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
+    var source = $('#list').html();
+    var template = Handlebars.compile(source);
 
-    $.ajax('data.json', {
-        success: function(response){
-            $.each(result, function(index, myObj) {
-                var msg = $("<p></p>");
-                msg.append("Price " + myObj.color);
-                msg.append("Qty " + myObj.value);
-                $('.myContainer').html(msg)
-            })
+    var context = {color: json, value: json};
+    var html = template(context);
 
-        },
-        error: function(request, errorType, errorMessage){
-            alert(errorType + " " + errorMessage);
-        }
-    });
-
-})
+    $('body').html(html);
+  });
+});
